c6ca0ebf2aff2c71d384c602a29f1fd3dde1e583
towerwatchman <towerwatchman@gmail.com>
2023-05-23 15:55:47 -0400

    Update api.py

--- api.py
+++ api.py
@@ -6,17 +6,20 @@ import json
 
 # TEST URL: https://f95zone.to/threads/the-necromancer-arises-prologue-whiteleaf-studio.154250/
 # TEST JSON: https://f95zone.to/sam/latest_alpha/latest_data.php?cmd=list&cat=games&page=1&sort=date&rows=90
-database_connection = database.REMOTE
+database_connection = database.LOCAL
 # Create Database for Atlas
 # CreateDatabase(database_connection)
 
 # Serializing json
-json_object = json.dumps(downloadBase(database_connection), default=str)
+# json_object = json.dumps(downloadBase(database_connection), default=str)
 
 # Writing to sample.json
-with open("sample.json", "w") as outfile:
-    outfile.write(json_object)
+# with open("sample.json", "w") as outfile:
+#    outfile.write(json_object)
 
 # f95.downloadThreadSummary(f95, download.NEW, False, database_connection)
 
 # Create bin object with latest data
+data = downloadBase(database_connection)
+for x in data:
+    print(x)
