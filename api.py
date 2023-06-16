3551b94e3c8ff8b0db85e5959a178290ba02a0ea
towerwatchman <towerwatchman@gmail.com>
2023-06-16 07:45:00 -0400

    added check for local or remote

--- api.py
+++ api.py
@@ -1,19 +1,25 @@
+from sys import platform
 from scraper.agents.f95 import *
 from scraper.utils.directory_manager import *
 from scraper.types.eTypes import *
 from scraper.utils.packager import *
 
-# Set database type: local is pc, remote is server
-database_connection = database.REMOTE
+# Set database type: local is pc (Windows), remote is server (Linux)
+# Will need to change eventually to check db type as well. This will be a function
+if platform == "win32":
+    database_connection = database.LOCAL
+else:
+    database_connection = database.REMOTE
 
 # Create folders: local is windows, remote is linux
 createDirectories(database_connection)
 
-# Create Database for Atlas
+# Create Database for Atlas: db will create based on sytem
 CreateDatabase(database_connection)
 
-# Download data from f95
+# Download from sources
+# F95 : 1st Source
 f95.downloadThreadSummary(f95, download.NEW, True, database_connection)
 
-# Package data. if new then create small update
+# Package data based on date. As of right now it will output a full db dump.
 packager.createPackage(database_connection)
